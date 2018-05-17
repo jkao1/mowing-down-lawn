7bf3e7ca98d37b4336606b26aa2fbab3b865f6be
Jason Kao <jasonkao85@gmail.com>
2018-05-17 14:30:35 -0400

    p sure im done

--- script.py
+++ script.py
@@ -37,12 +37,84 @@ def run(filename):
     screen = new_screen()
     zbuffer = new_zbuffer()
     tmp = []
+    polygons = []
     step_3d = 20
 
     p = mdl.parseFile(filename)
 
-    if p:
-        (commands, symbols) = p
-    else:
+    if not p:
         print "Parsing failed."
         return
+
+    (commands, symbols) = p
+    for cmd in commands:
+        name = cmd[0]
+        if name == "push":
+            stack.append([ x[:] for x in stack[-1] ])
+        elif name == "pop":
+            stack.pop()
+        elif name == "display":
+            display( screen )
+        elif name == "save":
+            save_extension( screen, cmd[1] + cmd[2] )
+
+        elif name in "spheretorusbox":
+            if name == "sphere":
+                add_sphere(
+                    polygons,
+                    float(cmd[1]), float(cmd[2]), float(cmd[3]),
+                    float(cmd[4]),
+                    step_3d,
+                )
+            elif name == "torus":
+                add_torus(
+                    polygons,
+                    float(cmd[1]), float(cmd[2]), float(cmd[3]),
+                    float(cmd[4]), float(cmd[5]),
+                    step_3d,
+                )
+            elif name == "box":
+                add_box(
+                    polygons,
+                    float(cmd[1]), float(cmd[2]), float(cmd[3]),
+                    float(cmd[4]), float(cmd[5]), float(cmd[6]),
+                )
+            matrix_mult( stack[-1], polygons )
+            draw_polygons(
+                polygons,
+                screen,
+                zbuffer,
+                view, ambient, light,
+                areflect, dreflect, sreflect
+            )
+            polygons = []
+
+        elif name == "line":
+            add_edge(
+                edges,
+                float(cmd[1]), float(cmd[2]), float(cmd[3]),
+                float(cmd[4]), float(cmd[5]), float(cmd[6]),
+            )
+            matrix_mult( stack[-1], edges )
+            draw_lines( edges, screen, zbuffer, color )
+            edges = []
+
+        elif name in "movescalerotate":
+            if name == "move":
+                transformer = make_translate(
+                    float(cmd[1]), float(cmd[2]), float(cmd[3])
+                )
+            elif name == "scale":
+                transformer = make_scale(
+                    float(cmd[1]), float(cmd[2]), float(cmd[3])
+                )
+            elif name == "rotate":
+                theta = float(cmd[2]) * math.pi / 180
+                if cmd[1] == "x":
+                    transformer = make_rotX(theta)
+                elif cmd[1] == "y":
+                    transformer = make_rotY(theta)
+                elif cmd[1] == "z":
+                    transformer = make_rotZ(theta)
+            matrix_mult( stack[-1], transformer )
+            stack[-1] = [ x[:] for x in transformer ]
